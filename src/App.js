395dbf054be9f6ab9933d6db0494f38a5e8e7ecc
Morgan <72710813+MCpracticesforskills@users.noreply.github.com>
2023-01-04 19:05:25 -0800

    will fix css later, but added link to my website & the GitHub repository
    Also fixed target_blank by adding a    rel="noopener noreferrer"

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/MCpracticesforskills/humidity"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced on GitHub
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://endearing-pika-93dc27.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Morgan Coyle
+        </a>
+      </footer>
     </div>
   );
 }
