04cb055bd59a263b486f27c2825747d451d2ea9f
Morgan <72710813+MCpracticesforskills@users.noreply.github.com>
2023-01-06 18:56:06 -0800

    deleted the Weather App h1 because it is self explanatory
    the city will be the new h1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Weather.css";
+import "./Conditions.css";
 import Conditions from "./Conditions";
 
 function App() {
